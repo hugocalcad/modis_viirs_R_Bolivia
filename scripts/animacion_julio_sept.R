c5e32c9411a8633f461ea77d5d49661d44e087a7
Victor Hugo <hugocalcad@gmail.com>
2019-09-18 22:55:02 -0400

    fixing animation con tmaps

--- scripts/animacion_julio_sept.R
+++ scripts/animacion_julio_sept.R
@@ -8,6 +8,7 @@ library(tidyverse)
 library(here)
 library(ggplot2)
 library(lubridate)
+library(tmap)
 
 # load .rdas
 load('data/modis_all_09092019.Rda')
@@ -21,28 +22,63 @@ str(viirs_final)
 modis_final$acq_date <- as.Date(modis_final$acq_date, format = '%Y-%m-%d')
 viirs_final$acq_date <- as.Date(viirs_final$acq_date, format = '%Y-%m-%d')
 
+#Abrimos la capa y hacemos subset
 sf_boldep <- st_read('shp/Bolivia_Departaentos.shp')
+#Subset para cubrir el area de Santa Cruz y Beni
+sf_SC_beni <- subset(sf_boldep,sf_boldep$NOM_DEP %in% c('SANTA CRUZ','BENI'))
+
+bbox_sc_be <- st_bbox(sf_SC_beni)
 
 #viirs select 1 de julio al 9 de septiembre
 viirs_julio_sep <- viirs_final %>%
   filter(acq_date > '2019-06-30') %>% 
   st_as_sf(coords = c("longitude", "latitude"), 
-           crs = 4326, agr = "constant")
+           crs = 4326, agr = "constant") %>%
+  st_crop(bbox_sc_be)
 
+##Establecemos las Fechas de inicio y de final
 start <- as.Date("2019-07-01",format="%Y-%m-%d")
 end   <- as.Date("2019-09-09",format="%Y-%m-%d")
+
+
+tmap_mode('plot')
+
+c_osm <- tmaptools::read_osm(tmaptools::bb(sf_SC_beni), ext = 1.5)
 
 ### \____ Function for GIF ----
 plot.map.calor <- function(){
   theDate <- start
   while(theDate <= end) {
-    dia <- viirs_julio_sep %>%
-      filter(acq_date == theDate)
-    mapa <- ggplot() +
-      geom_sf(data = sf_boldep)+
-      geom_sf(data = dia,  fill = "black", color = "red", alpha = .2) +
-      ggtitle(theDate)
+    if(theDate == start){  
+      dia <- viirs_julio_sep %>%
+        filter(acq_date == theDate)
+      ant <- dia
+    }else{
+      dia <- viirs_julio_sep %>%
+        filter(acq_date %in% c(theDate - 1, theDate))
+      ant <- viirs_julio_sep %>%
+        filter(acq_date < theDate)
+    }
+    # mapa <- ggplot() +
+    #   geom_sf(data = sf_boldep)+
+    #   geom_sf(data = dia,  fill = "black", color = "red", alpha = .2) +
+    #   ggtitle(paste0('Dia :', theDate))
+    mapa <-  tm_shape(c_osm) +
+      tm_rgb()  +
+      tm_shape(sf_SC_beni) + 
+      tm_polygons(alpha = 0,border.col = "black", lwd = 2)+
+      tm_shape(ant) +
+      tm_symbols(col = 'orange', shape = 15, scale = 0.35, alpha = 0.8 ) +
+      tm_shape(dia) +
+      tm_symbols(col = 'red', scale = 0.5, shape = 15 ) +
+      tm_layout(main.title = "Fuegos: Julio - Septiembre",
+                main.title.position = "center",
+                main.title.color = "blue",
+                panel.labels = c(paste0('Dia :', theDate)),
+                panel.label.color = "purple",
+                legend.text.color = "brown")
     theDate <- theDate + 1
+    message(paste0('Dia   ',theDate))
     print(mapa)
   }
 }
